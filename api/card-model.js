9e549048657b671f7238412e2800ebb097049504
Shalitha Senarathne <shalitha89@gmail.com>
2019-09-02 00:49:13 +0800

    card model updated to fetch data from json server

--- api/card-model.js
+++ api/card-model.js
@@ -1,20 +1,47 @@
-import { getData } from "./api.js";
+const url = "http://localhost:3000/cards";
 
-let {cards} = getData();
+export const getCards = async colmunId => {
+  let res = await fetch(url);
+  let cards = await res.json();
+  return cards.filter(card => {
+    return card.columnId === colmunId;
+  });
+};
 
-export const getCards = (colmunId) => {
-    return cards.filter(card => {
-        return card.columnId === colmunId;
-    });
-}
+export const createCard = async card => {
+  let res = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(card), // string or object
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  if (res.status >= 201 && res.status < 300) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-export const createCard = (card) => {
-    cards = [...cards, card];
-}
+export const updateCard = async card => {
+  let res = await fetch(url + `/${card.id}`, {
+    method: "PUT",
+    body: JSON.stringify(card), // string or object
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  let jsonRes = await res.json();
+  console.log(res);
+};
 
-export const updateCard = (card) => {
-    cards = cards.filter(c => {
-        return c.id !== card.id;
-    });
-    cards.push(card);
-}
+export const deleteCard = async card => {
+  let res = await fetch(url + `/${card.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  let jsonRes = await res.json();
+  console.log(res);
+};
